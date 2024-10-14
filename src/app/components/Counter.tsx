3028cb2ae8aa48bce1fcff17798938685667dd2c
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-10-14 12:35:08 +0500

    stored count value in local storage

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,14 +1,37 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaPlus, FaMinus, FaRedo } from 'react-icons/fa';
 
 
 export default function Counter() {
+  // State to store the count
   const [count, setCount] = useState(0);
+  // State to manage hydration status
+  const [isHydrated, setIsHydrated] = useState(false)
+
+  // Retrieve the count from local storage on component mount
+  useEffect(() => {
+    const savedCount = localStorage.getItem('count');
+    if (savedCount !== null) {
+      setCount(parseInt(savedCount, 10))
+    };
+    setIsHydrated(true) // Set hydrated to true after the count is set
+  }, [])
+
+  // Update local storae whenever the count changes
+  useEffect(() => {
+    if (isHydrated) {
+      localStorage.setItem('count', count.toString());
+    }
+  }, [count])
 
   const increment = () => setCount(count + 1);
   const decrement = () => setCount(count - 1);
   const reset = () => setCount(0);
+
+  // Render nothing until hydrated to avoid mismatch
+  if (!isHydrated) return null
+
   return (
     <div className="bg-white dark:bg-gray-900 p-8 md:p-12 rounded-3xl shadow-xl text-center max-w-sm w-full mx-auto md:max-w-md lg:max-w-lg">
       <h1 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-gray-800 dark:text-white mb-4">
